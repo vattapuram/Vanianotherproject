e9711d745c819128e16db0e9da29d46b57e425ef
vattapuram <vanirjava@gmail.com>
2016-04-22 17:03:05 -0700

    fghhjbvnnmm,

---
+++ src/com/vani/another/Another.java
@@ -0,0 +1,9 @@
+package com.vani.another;
+
+public class Another {
+
+	public static void main(String[] args) {
+System.out.println("Hello Everyone");
+	}
+
+}
---
+++ src/com/vani/another/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author vanir
+ *
+ */
+package com.vani.another;
